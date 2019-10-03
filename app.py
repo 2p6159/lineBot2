d4647e2bbdc1b582e1d09045b9b23ac2f9c94e54
2p6159 <56103511+2p6159@users.noreply.github.com>
2019-10-03 12:17:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ def callback():
     else :
         sendText(user,'ว่าใงน้ะ')
     return '',200
-  def sendText(user, text):
+  
+    def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
   headers = {
     'Content-Type': 'application/json; charset=UTF-8',
